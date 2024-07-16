93a7f99ff8259679070861b4075ab34574c55c71
Kyssuber <k22cong@siena.edu>
2024-07-15 19:43:12 -0500

    'final' version?

--- wiseseds/VFS_generate_input_files.py
+++ wiseseds/VFS_generate_input_files.py
@@ -16,33 +16,27 @@ def get_redshift(Vcosmic_array):
         z = np.asarray(Vcosmic_array)/3e5
     return z
         
-def trim_tables(IDs, redshifts, flux_table):
+def trim_tables(IDs, redshifts, flux_tab):
     
-    all_flags = (redshifts>0.) | (flux_table['photFlag'])
-    return IDs[all_flags], redshifts[all_flags], flux_table[all_flags]
+    all_flags = (redshifts>0.) & (flux_tab['photFlag'])
+    return IDs[all_flags], redshifts[all_flags], flux_tab[all_flags]
     
     
-def create_fauxtab(IDs, redshifts, flux_table, trim=True):
+def create_fauxtab(IDs, redshifts, flux_tab):
     
-    phot = flux_table
-    
-    if trim:
-        IDs, redshifts, flux_table = trim_tables(IDs,redshifts,phot)
-    
-    #isolate needed phot fluxes; convert from nanomaggies to mJy
-    #order: W1, W2, W3, W4, NUV, FUV, g, r
+    #isolate needed flux_tab fluxes; convert from nanomaggies to mJy
+    #order: FUV, NUV, g, r, W1, W2, W3, W4
+    fluxes = [flux_tab['FLUX_AP06_FUV']*3.631e-3, flux_tab['FLUX_AP06_NUV']*3.631e-3, flux_tab['FLUX_AP06_G']*3.631e-3,
+             flux_tab['FLUX_AP06_R']*3.631e-3, flux_tab['FLUX_AP06_W1']*3.631e-3, flux_tab['FLUX_AP06_W2']*3.631e-3,
+             flux_tab['FLUX_AP06_W3']*3.631e-3, flux_tab['FLUX_AP06_W4']*3.631e-3]
 
-    fluxes = [phot['FLUX_AP06_W1']*3.631e-3, phot['FLUX_AP06_W2']*3.631e-3, phot['FLUX_AP06_W3']*3.631e-3,
-             phot['FLUX_AP06_W4']*3.631e-3, phot['FLUX_AP06_NUV']*3.631e-3, phot['FLUX_AP06_FUV']*3.631e-3,
-             phot['FLUX_AP06_G']*3.631e-3, phot['FLUX_AP06_R']*3.631e-3]
-
-    flux_ivars = [phot['FLUX_IVAR_AP06_W1'], phot['FLUX_IVAR_AP06_W2'],
-                 phot['FLUX_IVAR_AP06_W3'], phot['FLUX_IVAR_AP06_W4'],
-                 phot['FLUX_IVAR_AP06_NUV'], phot['FLUX_IVAR_AP06_FUV'],
-                 phot['FLUX_IVAR_AP06_G'], phot['FLUX_IVAR_AP06_R']]
+    flux_ivars = [flux_tab['FLUX_IVAR_AP06_FUV'], flux_tab['FLUX_IVAR_AP06_NUV'],
+                 flux_tab['FLUX_IVAR_AP06_G'], flux_tab['FLUX_IVAR_AP06_R'],
+                 flux_tab['FLUX_IVAR_AP06_W1'], flux_tab['FLUX_IVAR_AP06_W2'],
+                 flux_tab['FLUX_IVAR_AP06_W3'], flux_tab['FLUX_IVAR_AP06_W4']]
     
-    flux_errs = [np.zeros(len(phot)),np.zeros(len(phot)),np.zeros(len(phot)),np.zeros(len(phot)),
-             np.zeros(len(phot)),np.zeros(len(phot)),np.zeros(len(phot)),np.zeros(len(phot))]
+    flux_errs = [np.zeros(len(flux_tab)),np.zeros(len(flux_tab)),np.zeros(len(flux_tab)),np.zeros(len(flux_tab)),
+             np.zeros(len(flux_tab)),np.zeros(len(flux_tab)),np.zeros(len(flux_tab)),np.zeros(len(flux_tab))]
 
     #for every list of fluxes...
     for index in range(len(flux_ivars)):
@@ -59,11 +53,16 @@ def create_fauxtab(IDs, redshifts, flux_table, trim=True):
     
     #create table to organize results
 
-    faux_table = Table([phot['VFID'],np.round(redshifts,4),fluxes[0],flux_errs[0],fluxes[1],flux_errs[1],
-                   fluxes[2],flux_errs[2],fluxes[3],flux_errs[3],fluxes[4],flux_errs[4],
-                   fluxes[5],flux_errs[5],fluxes[6],flux_errs[6],fluxes[7],flux_errs[7]],
-                   names=['VFID','redshift','WISE1','WISE1_err','WISE2','WISE2_err','WISE3','WISE3_err',
-                          'WISE4','WISE4_err','NUV','NUV_err','FUV','FUV_err','g','g_err','r','r_err'])
+    faux_table = Table([flux_tab['VFID'],np.round(redshifts,4),fluxes[0],
+                              flux_errs[0],fluxes[1],flux_errs[1],
+                              fluxes[2],flux_errs[2],fluxes[3],
+                              flux_errs[3],fluxes[4],flux_errs[4],
+                              fluxes[5],flux_errs[5],fluxes[6],
+                              flux_errs[6],fluxes[7],flux_errs[7]],
+                              names=['VFID','redshift','FUV','FUV_err','NUV','NUV_err',
+                                     'g', 'g_err', 'r', 'r_err',
+                                     'WISE1','WISE1_err','WISE2','WISE2_err','WISE3','WISE3_err','WISE4','WISE4_err'])
+    
     return faux_table
 
 def check_dir(north_path, south_path):
@@ -75,13 +74,16 @@ def check_dir(north_path, south_path):
         os.mkdir(homedir+'/Desktop/cigale_vf_south')
         print('Created '+ homedir + '/Desktop/cigale_vf_south')
 
-def create_input_files(IDs, redshifts, flux_table, north_path, south_path, trim=True):
+def create_input_files(IDs, redshifts, flux_tab, north_path, south_path, trim=True):
     
-    faux_table = create_fauxtab(IDs, redshifts, flux_table, trim=True)
+    if trim:
+        IDs, redshifts, flux_tab = trim_tables(IDs,redshifts,flux_tab)
+    
+    faux_table = create_fauxtab(IDs, redshifts, flux_tab)
     
     #I will need these flags as well. humph.
-    north_flag = phot['DEC_MOMENT']>32
-    south_flag = phot['DEC_MOMENT']<32
+    north_flag = flux_tab['DEC_MOMENT']>32
+    south_flag = flux_tab['DEC_MOMENT']<32
     
     #write files...
     
@@ -89,10 +91,11 @@ def create_input_files(IDs, redshifts, flux_table, north_path, south_path, trim=
     
     with open(homedir+'/Desktop/cigale_vf_north/vf_data_north.txt', 'w') as file:
         #create file header
-        s = '# id redshift FUV FUV_err NUV NUV_err g g_err r r_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
+        #s = '# id redshift FUV FUV_err NUV NUV_err BASS-g BASS-g_err BASS-r BASS-r_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
+        s = '# id redshift FUV FUV_err NUV NUV_err g_prime g_prime_err r_prime r_prime_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
         file.write(s)
 
-        #for every "good" galaxy in phot, add a row to the text file with relevant information
+        #for every "good" galaxy in flux_tab, add a row to the text file with relevant information
         for n in faux_table[north_flag]:
 
             s_gal = f"{n[0]} {n[1]} %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f"%(n[2],n[3],n[4],n[5],n[6],n[7],n[8],n[9],n[10],n[11],n[12],n[13],n[14],n[15],n[16],n[17]) + '\n'
@@ -102,10 +105,11 @@ def create_input_files(IDs, redshifts, flux_table, north_path, south_path, trim=
 
     with open(homedir+'/Desktop/cigale_vf_south/vf_data_south.txt', 'w') as file:
         #create file header
-        s = '# id redshift FUV FUV_err NUV NUV_err g g_err r r_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
+        #s = '# id redshift FUV FUV_err NUV NUV_err decamDR1-g decamDR1-g_err decamDR1-r decamDR1-r_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
+        s = '# id redshift FUV FUV_err NUV NUV_err g_prime g_prime_err r_prime r_prime_err WISE1 WISE1_err WISE2 WISE2_err WISE3 WISE3_err WISE4 WISE4_err'+' \n'
         file.write(s)
 
-        #for every "good" galaxy in phot, add a row to the text file with relevant information
+        #for every "good" galaxy in flux_tab, add a row to the text file with relevant information
         for n in faux_table[south_flag]:
 
             s_gal = f"{n[0]} {n[1]} %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f"%(n[2],n[3],n[4],n[5],n[6],n[7],n[8],n[9],n[10],n[11],n[12],n[13],n[14],n[15],n[16],n[17]) + '\n'
@@ -121,14 +125,14 @@ def create_ini_file(north_path, south_path):
     with open(north_path+'/pcigale.ini', 'w') as file:
         file.write('data_file = vf_data_north.txt \n')
         file.write('parameters_file = \n')
-        file.write('sed_modules = sfh2exp, bc03, nebular, dustatt_modified_CF00, dl2014, redshifting \n')
+        file.write('sed_modules = sfhdelayed, bc03, nebular, dustatt_modified_CF00, dale2014, redshifting \n')
         file.write('analysis_method = pdf_analysis \n')
         file.write('cores = 1 \n')
         file.close()    
     with open(south_path+'/pcigale.ini', 'w') as file:
         file.write('data_file = vf_data_south.txt \n')
         file.write('parameters_file = \n')
-        file.write('sed_modules = sfh2exp, bc03, nebular, dustatt_modified_CF00, dl2014, redshifting \n')
+        file.write('sed_modules = sfhdelayed, bc03, nebular, dustatt_modified_CF00, dale2014, redshifting \n')
         file.write('analysis_method = pdf_analysis \n')
         file.write('cores = 1 \n')
         file.close()    
@@ -150,11 +154,11 @@ def create_ini_file(north_path, south_path):
         file.write('cores = integer(min=1) \n')
         file.close()        
         
-def run_all(Vcosmic_array, IDs, flux_table, north_path, south_path, trim=True):
+def run_all(Vcosmic_array, IDs, flux_tab, north_path, south_path, trim=True):
 
     create_ini_file(north_path, south_path)
     redshifts = get_redshift(Vcosmic_array)
-    create_input_files(IDs, redshifts, flux_table, north_path, south_path, trim)
+    create_input_files(IDs, redshifts, flux_tab, north_path, south_path, trim)
 
         
 if __name__ == "__main__":
@@ -166,12 +170,11 @@ if __name__ == "__main__":
     
     #load tables
     vf = Table.read(homedir+'/Desktop/v2-20220820/vf_v2_environment.fits')
-    phot = Table.read(homedir+'/Desktop/v2-20220820/vf_v2_legacy_ephot.fits')
+    flux_tab = Table.read(homedir+'/Desktop/v2-20220820/vf_v2_legacy_ephot.fits')
     
     Vcosmic_array = vf['Vcosmic']
     IDs = vf['VFID']
-    flux_table = phot
     
-    run_all(Vcosmic_array, IDs, flux_table, north_path, south_path, trim)
+    run_all(Vcosmic_array, IDs, flux_tab, north_path, south_path, trim)
     
         
--- wiseseds/run_cigale.py
+++ wiseseds/run_cigale.py
@@ -5,24 +5,31 @@ homedir = os.getenv("HOME")
 
 import sys
 import fileinput
+import re
 
-def run_genconf(dir_path, sed_plots=False):
+def run_genconf(dir_path):
     os.chdir(dir_path)
-    os.system('pcigale genconf')  #will generate configuration files pcigale.ini and pcigale.ini.spec
-                                  #can check/edit the various parameters in the file before the next step
-    
-    #the pcigale genconf default save_best_sed to False...however, we need these files in order to generate plots!
-    if sed_plots:
-        
-        for line in fileinput.input(dir_path, inplace=True):
-            if line.startswith('save_best_sed'):
-                #quite curious...the print statement is what the line will change to!
-                print("save_best_sed = True")
-            else:
-                #this statement is included so that lines not including save_best_sed are ignored.
-                #if I remove this line, in fact, the pcigale.ini file will ONLY contain the "save_best_sed = True" text.
-                print(line.strip()) 
-                
+    #will generate configuration files pcigale.ini and pcigale.ini.spec
+    #can check/edit the various parameters in the file before the next step
+    os.system('pcigale genconf')
+
+def change_sedplot(dir_path):
+    with open(dir_path+'/pcigale.ini', 'r') as file:
+        lines = file.readlines()
+
+    # Modify the lines
+    modified_lines = []
+    for line in lines:
+        if re.match(r'^\s*save_best_sed\s*=', line):
+            modified_lines.append("save_best_sed = True\n")
+            print('line changed: save_best_sed = True')
+        else:
+            modified_lines.append(line)
+
+    # Write the modified lines back to the file
+    with open(dir_path+'/pcigale.ini', 'w') as file:
+        file.writelines(modified_lines)
+
 def run_cigale(dir_path):
 
     os.chdir(dir_path)   #just in case...
@@ -44,17 +51,29 @@ if __name__ == "__main__":
     if '-dir_path' in sys.argv:
         p = sys.argv.index('-dir_path')
         dir_path = str(sys.argv[p+1])
+        print(dir_path)
     
     if '-sed_plots' in sys.argv:
         sed_plots = True
+    else:
+        sed_plots = False
     
     print('Configuring input text files...')
-    run_genconf(dir_path, sed_plots)
-    print('Fin!')
-    print('Execuring CIGALE...')
+    run_genconf(dir_path)
+    if sed_plots:
+        change_sedplot(dir_path)   #changes line to save_best_sed = True 
+    print('Executing CIGALE...')
     run_cigale(dir_path)
     print('Fin!')
     if sed_plots:
         print('Generating SED plots...')
         run_sed_plots(dir_path)
+        print('Organizing output...')
+        os.chdir(dir_path+'/out/')
+        os.mkdir('SFH_outputs')
+        os.mkdir('best_SED_models')
+        os.system('mv *best_model* best_sed_models')
+        os.system('mv *_SFH* SFH_outputs')
         print('Fin!')
+        
+        
